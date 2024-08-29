0ff26008ccd63a9aafe5255f8738cc0f62bcf17e
Nikesh Singh <nikesh.pathania@gmail.com>
2024-08-29 13:39:35 +0000

    Defining Component Input

--- 01-starting-project/src/app/users/users.component.ts
+++ 01-starting-project/src/app/users/users.component.ts
@@ -1,8 +1,4 @@
-import { Component, computed, Input, signal } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-// Intialised Index when component is load
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-
+import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-users',
   standalone: true,
@@ -11,26 +7,13 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './users.component.css',
 })
 export class UsersComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
-
-  currentIndex: number = 0;
-
   @Input() avatar!: string;
+  @Input() name!: string;
 
-  // get imagePath() {
-  //   // Get Image Path through getter
-  //   return 'assets/users/' + this.selectedUser.avatar;
-  // }
-
-  selectUser() {
-    // Rediclare randomIndex for scope based when click on selectUser method
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  get imagePath() {
+    // Get Image Path through getter
+    return 'assets/users/' + this.avatar;
   }
 
-  /*selectUser() {
-    this.selectedUser.set(DUMMY_USERS[this.currentIndex]);
-    this.currentIndex = (this.currentIndex + 1) % DUMMY_USERS.length;
-  }*/
+  onSelectUser() {}
 }
