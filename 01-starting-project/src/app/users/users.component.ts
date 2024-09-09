37c87db2e8b19159dddb510c76bcc1eef14674ad
Nikesh Singh <nikesh.pathania@gmail.com>
2024-09-09 06:31:05 +0000

    39-Accespting-Objects-As-Inputs-&-Adding-Appropraiate-Typing

--- 01-starting-project/src/app/users/users.component.ts
+++ 01-starting-project/src/app/users/users.component.ts
@@ -14,18 +14,24 @@ import {
   styleUrl: './users.component.css',
 })
 export class UsersComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  // @Input({ required: true }) id!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+
+  @Input({ required: true }) user!: {
+    id: string;
+    avatar: string;
+    name: string;
+  };
 
   @Output() select = new EventEmitter();
 
   // Get Is used for getter Computed image
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
