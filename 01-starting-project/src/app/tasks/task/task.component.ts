99415d2df2f8b0d0b96d0ae46ff1ab4d6b7f5a9c
Nikesh Singh <nikesh.pathania@gmail.com>
2024-09-09 13:05:31 +0000

    add userId in task component ts file

--- 01-starting-project/src/app/tasks/task/task.component.ts
+++ 01-starting-project/src/app/tasks/task/task.component.ts
@@ -1,5 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
+type tasks = {
+  id: string;
+  userId: string;
+  title: string;
+  summary: string;
+  dueDate: string;
+};
 @Component({
   selector: 'app-task',
   standalone: true,
@@ -7,4 +14,6 @@ import { Component } from '@angular/core';
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
-export class TaskComponent {}
+export class TaskComponent {
+  @Input({ required: true }) task!: tasks;
+}
