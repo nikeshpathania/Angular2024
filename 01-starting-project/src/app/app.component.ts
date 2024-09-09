729fb1a585d23067a953af53e95c1fd84e5aa071
Nikesh Singh <nikesh.pathania@gmail.com>
2024-09-09 11:03:59 +0000

    44-Adding-More-Components-Demo-App

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -3,13 +3,11 @@ import { HeaderComponent } from './header/header.component';
 import { UsersComponent } from './users/users.component';
 import { DUMMY_USERS } from './dummy-users';
 import { TasksComponent } from './tasks/tasks.component';
-import { NgFor } from '@angular/common';
-import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UsersComponent, TasksComponent, NgFor, NgIf],
+  imports: [HeaderComponent, UsersComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
